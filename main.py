1d0875c1beab7878f8d79c0b1a021469c0dc4aa7
Kumar Sambhav <krsambhav16@gmail.com>
2022-04-26 17:36:24 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import onclick
+import pyotp
+import pyperclip
+from pynput import mouse
+import pyautogui
+
+def on_click(x,y, button, pressed):
+  skey = pyperclip.paste()
+  totp = pyotp.TOTP(skey)
+  print(button)
+  if button == mouse.Button.middle and pressed:
+    pyautogui.write(totp.now())
+    print(skey, totp.now())
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+listener.join()
