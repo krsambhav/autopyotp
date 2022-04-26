8eaed8ff606a21b9936060c224253df7f3348846
Kumar Sambhav <krsambhav16@gmail.com>
2022-04-26 17:56:04 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ a python tool to paste time based otp from secret key without hassles
 
 `python3 main.py`
 
-* Copy secret key to your clipboard and press the scroll button while focusing on the input box where you want to paste the OTP.
+* Copy secret key to your clipboard and **press the scroll button / right arrow key** while focusing on the input box where you want to paste the OTP.
 
 >NOTE : If you want to modify the button (in case you don't have a scroll button) then you can change the button on line 10.
 
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pyotp
 import pyperclip
 from pynput import mouse
+from pynput import keyboard
 import pyautogui
 
 def on_click(x,y, button, pressed):
   skey = pyperclip.paste()
   totp = pyotp.TOTP(skey)
-  print(button)
   if button == mouse.Button.middle and pressed:
     pyautogui.write(totp.now())
     print(skey, totp.now())
 
+def on_press(key):
+  skey = pyperclip.paste()
+  totp = pyotp.TOTP(skey)
+  if key == keyboard.Key.right:
+    pyautogui.write(totp.now())
+    print(skey, totp.now())
+
 listener = mouse.Listener(on_click=on_click)
+listener = keyboard.Listener(on_press=on_press)
 listener.start()
 listener.join()
